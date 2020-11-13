b78b00201138154482c272285cb3690feaafb448
sungeun101 <josungeun1019@gmail.com>
2020-11-13 16:31:37 +0900

    show popular video titles

---
+++ src/app.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import './app.css';
+import VideoList from './components/video_list/video_list';
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDooq3BRo08juGN4qVUK5lubvTASkjqr24',
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos} />;
+}
+
+export default App;
